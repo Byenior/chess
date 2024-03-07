de3674cbe7bc3f8175034fbba0e49563ade4ec74
Nathaphong Wongsri <Nathaphong.won@gmail.com>
2024-03-07 16:22:18 +0700

    yeahya

--- myMine.js
+++ myMine.js
@@ -1,37 +1,10 @@
-let slots = document.querySelectorAll('td');
-slots.forEach(slot => {
-if (slot.style.backgroundColor == 'white') {
-}
+let tds = document.querySelectorAll('td');
+tds.forEach(td => {
+    td.addEventListener('click', function() {
 
+
+        
+        // img.style.backgroundColor = 'red'
+        //     console.log('Clicked on an image with white background!');
+    });
 });
-
-
-function allowDrop(ev) {
-    // console.log(ev)
-    // if (ev.target.style.backgroundColor === 'white') {
-        ev.preventDefault();
-    // }
-}
-
-function drag(ev) {
-    // Set the data being dragged to the image itself
-    ev.dataTransfer.setData("text/plain", ev.target.outerHTML);
-    // Add a class to the source td
-    ev.target.parentElement.classList.add('dragging');
-}
-
-
-function drop(ev) {
-    ev.preventDefault();
-    // Get the data from the drag event
-    var data = ev.dataTransfer.getData("text/plain");
-    // Clear the inner HTML of the target td
-    ev.target.innerHTML = '';
-    // Append the dragged image to the target td
-    ev.target.innerHTML = data;
-    // Clear the inner HTML of the source td
-    var sourceTd = document.querySelector('.dragging');
-    if (sourceTd) {
-        sourceTd.innerHTML = '';
-    }
-}
