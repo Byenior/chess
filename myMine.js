178af8030d9fead036c8bf556c967764d80d5a7a
Nathaphong Wongsri <Nathaphong.won@gmail.com>
2024-03-08 15:13:06 +0700

    b1

--- myMine.js
+++ myMine.js
@@ -1,105 +1,92 @@
-let tds = document.querySelectorAll('td');
-tds.forEach(td => {
-    td.addEventListener('click', function() {
+let tds = document.querySelectorAll("td");
+tds.forEach((td) => {
+  td.addEventListener("click", function () {
+    let parentTd = this.parentNode;
+    // console.log(parentTd)
 
+    let childrenDivs = td.querySelector("div");
+    let body = childrenDivs.querySelector("img");
+    let corp = childrenDivs.querySelector("div");
 
-        let parentTd = this.parentNode;
-        // console.log(parentTd)
-
-        
-        let childrenDivs = td.querySelector('div');
-        let body = childrenDivs.querySelector('img');
-        let corp = childrenDivs.querySelector('div');
-
-
-        if (corp.style.display != 'flex') {
-            corp.style.display = 'flex'
-            body.style.backgroundColor = 'red'
-
-        } else {
-            corp.style.display = 'none'
-            body.style.backgroundColor = 'transparent'
-
-        }
-    });
+    if (corp.style.display != "flex") {
+      socket.emit("action", corp, body);
+      socket.on("action", corp, body);
+    } else {
+      socket.emit("action", corp, body);
+      socket.on("action", corp, body);
+    }
+  });
 });
 
-
-
 let batmanSide = [
-    document.getElementById('1'),
-    document.getElementById('3'),
-    document.getElementById('5'),
-    document.getElementById('7'),
-    document.getElementById('10'),
-    document.getElementById('12'),
-    document.getElementById('14'),
-    document.getElementById('16'),]
-    
+  document.getElementById("1"),
+  document.getElementById("3"),
+  document.getElementById("5"),
+  document.getElementById("7"),
+  document.getElementById("10"),
+  document.getElementById("12"),
+  document.getElementById("14"),
+  document.getElementById("16"),
+];
+
 let jokerSide = [
-document.getElementById('49'),
-document.getElementById('51'),
-document.getElementById('53'),
-document.getElementById('55'),
-document.getElementById('58'),
-document.getElementById('60'),
-document.getElementById('62'),
-document.getElementById('64'),]
+  document.getElementById("49"),
+  document.getElementById("51"),
+  document.getElementById("53"),
+  document.getElementById("55"),
+  document.getElementById("58"),
+  document.getElementById("60"),
+  document.getElementById("62"),
+  document.getElementById("64"),
+];
 
+function createChess(srcImg, id, where) {
+  let chessPiece = document.createElement("div");
+  let img = document.createElement("img");
+  img.id = id;
+  if (id < 10) {
+    img.classList = "batman";
+  } else {
+    img.classList = "joker";
+  }
+  img.src = srcImg;
+  img.draggable = false;
+  let infoDiv = document.createElement("div");
+  infoDiv.classList.add("corp");
+  let p1 = document.createElement("p");
+  p1.textContent = "attack";
+  let p2 = document.createElement("p");
+  p2.textContent = "skill";
+  let p3 = document.createElement("p");
+  p3.textContent = "move";
+  let p4 = document.createElement("p");
+  p4.textContent = "sleep";
 
-function createChess(srcImg,id,where) {
-    let chessPiece = document.createElement('div');
-    let img = document.createElement('img');
-    img.id = id;
-    if (id < 10) {
-        img.classList = 'batman'
-    } else {
-        img.classList = 'joker'
-    }
-    img.src = srcImg;
-    img.draggable = false;
-    let infoDiv = document.createElement('div');
-    infoDiv.classList.add('corp');
-    let p1 = document.createElement('p');
-    p1.textContent = 'attack';
-    let p2 = document.createElement('p');
-    p2.textContent = 'skill';
-    let p3 = document.createElement('p');
-    p3.textContent = 'move';
-    let p4 = document.createElement('p');
-    p4.textContent = 'sleep';
-    
-    infoDiv.appendChild(p1);
-    infoDiv.appendChild(p2);
-    infoDiv.appendChild(p3);
-    infoDiv.appendChild(p4);
-    
-    chessPiece.appendChild(img);
-    chessPiece.appendChild(infoDiv);
-    
-    where.appendChild(chessPiece);
+  infoDiv.appendChild(p1);
+  infoDiv.appendChild(p2);
+  infoDiv.appendChild(p3);
+  infoDiv.appendChild(p4);
 
+  chessPiece.appendChild(img);
+  chessPiece.appendChild(infoDiv);
+
+  where.appendChild(chessPiece);
 }
 
-    
-setInterval(() => createChess('asset/2.png',1,batmanSide[0]) ,500)
-setInterval(() => createChess('asset/1.png',2,batmanSide[1]) ,1000)
-setInterval(() => createChess('asset/2.png',3,batmanSide[2]) ,1500)
-setInterval(() => createChess('asset/3.png',4,batmanSide[3]) ,2000)
-setInterval(() => createChess('asset/4.png',5,batmanSide[4]) ,2000)
-setInterval(() => createChess('asset/3.png',6,batmanSide[5]) ,1500)
-setInterval(() => createChess('asset/5.png',7,batmanSide[6]) ,1000)
-setInterval(() => createChess('asset/4.png',8,batmanSide[7]) ,500)
+setInterval(() => createChess("asset/2.png", 1, batmanSide[0]), 500);
+setInterval(() => createChess("asset/1.png", 2, batmanSide[1]), 1000);
+setInterval(() => createChess("asset/2.png", 3, batmanSide[2]), 1500);
+setInterval(() => createChess("asset/3.png", 4, batmanSide[3]), 2000);
+setInterval(() => createChess("asset/4.png", 5, batmanSide[4]), 2000);
+setInterval(() => createChess("asset/3.png", 6, batmanSide[5]), 1500);
+setInterval(() => createChess("asset/5.png", 7, batmanSide[6]), 1000);
+setInterval(() => createChess("asset/4.png", 8, batmanSide[7]), 500);
 
-setInterval(() => createChess('asset/12.png',11,jokerSide[0]) ,500)
-setInterval(() => createChess('asset/11.png',12,jokerSide[1]) ,1000)
-setInterval(() => createChess('asset/12.png',13,jokerSide[2]) ,1500)
-setInterval(() => createChess('asset/13.png',14,jokerSide[3]) ,2000)
-setInterval(() => createChess('asset/14.png',15,jokerSide[4]) ,2000)
-setInterval(() => createChess('asset/13.png',16,jokerSide[5]) ,1500)
-setInterval(() => createChess('asset/14.png',17,jokerSide[6]) ,1000)
-setInterval(() => createChess('asset/15.png',18,jokerSide[7]) ,500)
-
-
-
-
+setInterval(() => createChess("asset/12.png", 11, jokerSide[0]), 500);
+setInterval(() => createChess("asset/11.png", 12, jokerSide[1]), 1000);
+setInterval(() => createChess("asset/12.png", 13, jokerSide[2]), 1500);
+setInterval(() => createChess("asset/13.png", 14, jokerSide[3]), 2000);
+setInterval(() => createChess("asset/14.png", 15, jokerSide[4]), 2000);
+setInterval(() => createChess("asset/13.png", 16, jokerSide[5]), 1500);
+setInterval(() => createChess("asset/14.png", 17, jokerSide[6]), 1000);
+setInterval(() => createChess("asset/cat.gif", 18, jokerSide[7]), 500);
--- server_mymine.js
+++ server_mymine.js
@@ -1,57 +1,36 @@
-const express = require('express');
-const http = require('http');
-const path = require('path');
+const express = require("express");
+const http = require("http");
+const path = require("path");
 const { Server } = require("socket.io");
-const cors = require('cors');
-const ngrok = require("@ngrok/ngrok");
 
 const app = express();
 const server = http.createServer(app);
-// const io = new Server(server);
-const link = 'http://localhost:7777/';
+const io = new Server(server);
 
-const io = new Server(server,{
-    cors: {
-      origin: link,
-      methods: ["GET", "POST"]
-    }
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("message", (msg) => {
+    console.log(msg);
+    io.emit("message", msg);
   });
-  
-app.use(cors({
-    origin: link
-}));
+
+  socket.on("action", (msg) => {
+    console.log(msg);
+    io.emit("action", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+});
 
 app.use(express.static(path.join(__dirname)));
 
-app.get('/', (req, res) => {
-    // res.header("Access-Control-Allow-Origin", "*");
-    res.sendFile(path.join(__dirname, 'field.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "field.html"));
 });
-
-
-io.on("connection", (socket) => {
-    console.log("A user connected");
-    socket.on("message", (msg) => {
-        console.log(msg)
-        socket.emit("message", msg);
-
-
-    });
-    socket.on("disconnect", () => {
-        console.log("A user disconnected");
-    });
-});
-
 
 server.listen(7777, () => {
-    console.log('server running at http://localhost:7777')
+  console.log("server running at http://localhost:7777");
 });
-
-(async function () {
-	const listener = await ngrok.connect({
-		addr: link,
-		authtoken: '2Ng1GDirVyNZ8rPhUgqhr78ZspR_4EvdRptmmcAK6y1K56kyL',
-	});
-
-	console.log(`Ngrok URL: ${listener}`);
-})();
